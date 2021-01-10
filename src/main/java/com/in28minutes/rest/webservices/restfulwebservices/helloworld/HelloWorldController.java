61384052615b0441b474a23cb134ba900f8b7e2e
nikhilmasthipur <nikhilmasthipur01@gmail.com>
2021-01-10 14:44:04 -0500

    Added a second commit

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -20,6 +20,7 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		System.out.println("this is docker project");
+		System.out.println("this is second commit");
 		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 }
